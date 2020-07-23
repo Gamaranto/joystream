ec671ed8d90b9ad0a164b5c7c76493b6c7c6bf5f
Francesco Baccetti <gamarantor@gmail.com>
2020-07-23 21:30:58 +0200

    Integrate Mocked Data With Atlas

--- atlas/packages/app/src/App.tsx
+++ atlas/packages/app/src/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { Router } from '@reach/router'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 import store from './store'
 import { Layout } from './components'
 import { HomeView } from './views'
 
+const client = new ApolloClient({
+	uri: 'http://localhost:9002/graphql',
+	cache: new InMemoryCache(),
+})
 export default function App() {
 	return (
-		<Provider store={store}>
-			<Layout>
-				<Router primary={false}>
-					<HomeView default />
-				</Router>
-			</Layout>
-		</Provider>
+		<ApolloProvider client={client}>
+			<Provider store={store}>
+				<Layout>
+					<Router primary={false}>
+						<HomeView default />
+					</Router>
+				</Layout>
+			</Provider>
+		</ApolloProvider>
 	)
 }
